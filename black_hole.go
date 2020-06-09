63361742044babcc144e26c59fa21ea248ac345e
Pasha Radchenko <ep4sh2k@mail.com>
2020-06-09 22:13:37 +0300

    Add uploadFile using html form

--- black_hole.go
+++ black_hole.go
@@ -3,30 +3,12 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"time"
 )
-
-func Output(w http.ResponseWriter, r *http.Request) {
-	base_path, ok := os.LookupEnv("BH_PATH")
-	if !ok {
-		log.Println("Env BH_PATH was not found...")
-		log.Println("Creating default /tmp/bh/ ...")
-		os.Mkdir("/tmp/bh/", 0755)
-		base_path = "/tmp/bh/"
-	}
-	tempfile := base_path + GenFileName()
-	file, err := SaveFile(tempfile)
-	if err != nil {
-		log.Fatalf("%+v\n", err)
-	}
-	fmt.Printf("%s", file.Name())
-	io.WriteString(w, "Saved file: "+file.Name()+"\n")
-
-}
 
 func GenFileName() string {
 	now := time.Now().String()
@@ -34,21 +16,44 @@ func GenFileName() string {
 	return filename
 }
 
-func SaveFile(tempfile string) (*os.File, error) {
-	// accept my bytes
-
+func SaveFile(tempfile string, b []byte) (*os.File, error) {
 	f, err := os.OpenFile(tempfile, os.O_RDWR|os.O_CREATE, 0755)
 	defer f.Close()
 	if err != nil {
 		log.Fatalf("%+v\n", err)
 	}
-	// take you bytes here
-	s := []byte("Kitzen the CAT!")
-	f.Write(s)
+
+	f.Write(b)
 	return f, nil
 }
 
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	basePath, ok := os.LookupEnv("BH_PATH")
+	if !ok {
+		log.Println("Env BH_PATH was not found...")
+		log.Println("Creating default /tmp/bh/ ...")
+		os.Mkdir("/tmp/bh/", 0755)
+		basePath = "/tmp/bh/"
+	}
+	r.ParseMultipartForm(10 << 20)
+	uploadingFile, handler, err := r.FormFile("file")
+	uploadingBytes, err := ioutil.ReadAll(uploadingFile)
+	if err != nil {
+		log.Fatalf("%+v\n", err)
+		return
+	}
+	log.Printf("File Name:%s Size:%d Header:%s\n", handler.Filename, handler.Size, handler.Header)
+	tempfile := basePath + GenFileName() + handler.Filename
+	file, err := SaveFile(tempfile, uploadingBytes)
+	if err != nil {
+		log.Fatalf("%+v\n", err)
+	}
+
+	fmt.Printf("%s", file.Name())
+
+}
+
 func main() {
-	http.HandleFunc("/", Output)
+	http.HandleFunc("/u", uploadFile)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
